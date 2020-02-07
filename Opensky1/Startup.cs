c8557e783faddea284efe7f2aed98dbd23b14cea
thomsyne <oluwatomisinoluwafemi@gmail.com>
2020-02-07 05:04:55 +0000

    Initial Project Setup

---
+++ Opensky1/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Net;
+
+[assembly: OwinStartupAttribute(typeof(Opensky1.Startup))]
+namespace Opensky1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+        }
+    }
+}
